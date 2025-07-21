f0ca06af5c5b06754fa7ae89146db85ce5d2487c
宇寒 <149793500+YuHan1015@users.noreply.github.com>
2025-07-22 02:50:45 +0800

    1
    1

--- docs/.vuepress/.temp/internal/routes.js
+++ docs/.vuepress/.temp/internal/routes.js
@@ -1,11 +1,8 @@
 export const redirects = JSON.parse("{}")
 
 export const routes = Object.fromEntries([
-  ["/", { loader: () => import(/* webpackChunkName: "index.html" */"E:/我的程序大全.zip/简幻欢乐子文档/docs/.vuepress/.temp/pages/index.html.js"), meta: {"title":""} }],
-  ["/%E5%A6%82%E4%BD%95%E6%B7%BB%E5%8A%A0%E4%B9%90%E5%AD%90.html", { loader: () => import(/* webpackChunkName: "如何添加乐子.html" */"E:/我的程序大全.zip/简幻欢乐子文档/docs/.vuepress/.temp/pages/如何添加乐子.html.js"), meta: {"title":"如何添加乐子"} }],
   ["/lezi/", { loader: () => import(/* webpackChunkName: "lezi_index.html" */"E:/我的程序大全.zip/简幻欢乐子文档/docs/.vuepress/.temp/pages/lezi/index.html.js"), meta: {"title":"乐子列表"} }],
   ["/nav/", { loader: () => import(/* webpackChunkName: "nav_index.html" */"E:/我的程序大全.zip/简幻欢乐子文档/docs/.vuepress/.temp/pages/nav/index.html.js"), meta: {"title":"乐子导航"} }],
-  ["/nav/lezi-example/", { loader: () => import(/* webpackChunkName: "nav_lezi-example_index.html" */"E:/我的程序大全.zip/简幻欢乐子文档/docs/.vuepress/.temp/pages/nav/lezi-example/index.html.js"), meta: {"title":"这是一个示例乐子"} }],
   ["/404.html", { loader: () => import(/* webpackChunkName: "404.html" */"E:/我的程序大全.zip/简幻欢乐子文档/docs/.vuepress/.temp/pages/404.html.js"), meta: {"title":""} }],
 ]);
 
