2ac939089a3179f9912a50d525a6b028d06b8409
宇寒 <149793500+YuHan1015@users.noreply.github.com>
2025-07-22 18:12:24 +0800

    Update nav.ts

--- docs/.vitepress/configs/nav.ts
+++ docs/.vitepress/configs/nav.ts
@@ -3,6 +3,6 @@ import type { DefaultTheme } from 'vitepress'
 export const nav: DefaultTheme.Config['nav'] = [
   { text: '乐子导航', link: '/nav/' },
   { text: '简幻欢', link: 'https://simpfun.cn' },
-  { text: '想要新乐子？点我提交', link: 'https://github.com/YuHan1015/simpfun-lezi-docs/issuesn' },
-  { text: '准备好了你的乐子？点我PR', link: 'https://github.com/YuHan1015/simpfun-lezi-docs/pulls' },
+  { text: '反馈', link: 'https://github.com/YuHan1015/simpfun-lezi-docs/issues' },
+  { text: '上传新乐', link: 'https://github.com/YuHan1015/simpfun-lezi-docs/pulls' },
 ]
