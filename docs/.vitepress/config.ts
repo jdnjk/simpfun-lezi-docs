fc18c8a932f721816c678acce9e0ec05f3291f3f
宇寒 <149793500+YuHan1015@users.noreply.github.com>
2025-07-28 18:34:59 +0800

    1

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -8,7 +8,7 @@ const APP_BASE_PATH = basename(process.env.GITHUB_REPOSITORY || '')
 
 export default defineConfig({
   outDir: '../dist',
-  base: APP_BASE_PATH ? `/${APP_BASE_PATH}/` : '/',
+  base: process.env.VERCEL ? '/' : (APP_BASE_PATH ? `/${APP_BASE_PATH}/` : '/'),
 
   lang: 'zh-CN',
   title: '简幻欢乐子文档',
